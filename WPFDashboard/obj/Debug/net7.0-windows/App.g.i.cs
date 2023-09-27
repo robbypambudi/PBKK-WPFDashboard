e4e0283923a50a936e9d903cb2141f069875b904
Robby Pambudi <robby.pambudi10@gmail.com>
2023-09-27 13:34:53 +0700

    feat: solve tugas 4

--- WPFDashboard/obj/Debug/net7.0-windows/App.g.i.cs
+++ WPFDashboard/obj/Debug/net7.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace WPFDashboard {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/WPFDashboard;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPFDashboard;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
